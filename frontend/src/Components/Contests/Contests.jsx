d4a6977ffb79e5b48f20597b44301af7f45fd94d
ayushi pal <ayushipal06@gmail.com>
2024-07-11 11:54:41 +0530

    chore: Update Contests component and Register component UI

--- frontend/src/Components/Contests/Contests.jsx
+++ frontend/src/Components/Contests/Contests.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 const Contests = () => {
   return (
-    <div>
+    <>
+    <div className="d-flex m-5 justify-content-between align-items-center">
+      <h2 style={{"fontWeight":"600","fontSize": "30px"}}>Ongoing Fashion Contests</h2>
+      <h2 style={{"fontWeight":"400","fontSize": "20px"}}>See all</h2>
+      </div>
+
       
-    </div>
+    </>
   )
 }
 
--- frontend/src/Components/Register/Register.jsx
+++ frontend/src/Components/Register/Register.jsx
@@ -30,23 +30,39 @@ const Register = () => {
         <form onSubmit={handleSubmit}>
             <h2 className="my-5 fw-semibold">Personal Details</h2>
             {error && <p style={{ color: 'red' }}>{error}</p>}
-       
-        <p className="fs-4">Enter your email to sign up</p>
-             <div className="col-sm-10 mx-auto my-4">
-                    <input type="email" required style={{"backgroundColor":"#F0EBEB","padding":"12px 23px","width":"45%"}} class=" form-control m-auto fs-6" id="staticEmail" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="someone@example.com" />
-                    </div>
-         
-   
-           
-                    <div style={{"margin":"auto","text-align": "right","width":"37%"}}>
-                   
-                    </div>
-        
+          
+  <div class="row mb-4 w-50 m-auto">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">Name</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control " style={{"border":"1px solid black","outline":"none"}} id="inputEmail3" placeholder='Enter your full name'/>
+    </div>
+  </div>
+  <div class="row mb-4 w-50 m-auto">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Username</label>
+    <div class="col-sm-10">
+      <input type="text" class="form-control " style={{"border":"1px solid black","outline":"none"}} id="inputPassword3" placeholder='Enter your unique username'/>
+    </div>
+  </div>
+  <div class="row mb-4 w-50 m-auto">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control " style={{"border":"1px solid black","outline":"none"}} id="inputPassword3" placeholder='Enter your password'/>
+    </div>
+  </div>
 
-            <button type="submit" class="btn my-4" style={{"background":"black","color":"white","padding":"11px 29px","fontWeight":"500"}}>Send OTP</button>
-            <p >
-                Already have an account? <Link to="/signup" className="text-decoration-underline">Login here</Link>
-            </p>
+  <div class="row mb-4 w-50 m-auto">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Confirm Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control " style={{"border":"1px solid black","outline":"none"}} id="inputPassword3" placeholder='Confirm your password'/>
+    </div>
+  </div>
+
+
+
+  
+  <button type="submit" class="btn my-4" style={{"background":"black","color":"white","padding":"11px 29px","fontWeight":"500"}}>SUBMIT</button>
+
+       
         </form>
     );
 }
